e386818583c95a6b32e5575d28bd5d9211a47330
philo-lin <philolin@cs.utexas.edu>
2021-10-30 22:33:34 -0500

    frontend finished

--- astrotherapy/src/App.js
+++ astrotherapy/src/App.js
@@ -1,31 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Input, Button, Popconfirm, message } from 'antd';
 import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css';
+import "@fontsource/open-sans"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const { TextArea } = Input;
+
+
+function confirm(e) {
+  console.log(e);
+  message.success('ur depressed!');
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <NameForm />
-        </div>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hi Sumedh!
-        </p> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    <>
+      <NameForm></NameForm>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <div>
+    //       <NameForm />
+    //     </div>
+    //     {/* <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //       Hi Sumedh!
+    //     </p> 
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a> */}
+    //   </header>
+    // </div>
   );
 }
 
@@ -51,14 +66,51 @@ class NameForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Journal Entry:
-          <input type="text" id="txtfile" value={this.state.value} onChange={this.handleChange}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
+      <>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="col-8">
+              <div style={{padding: '14px'}}>
+                <div style={{ margin: 'auto' }}>
+                  <TextArea 
+                    style={{
+                      width: '100%',
+                      fontSize: '40px'
+                    }}
+                    rows={10} 
+                    cols={20}
+                    placeholder={'how are you feeling today?'}
+                    autoSize={{ minRows: 5, maxRows: 15}}
+                    bordered={false}
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col-2">
+              <div class="float-end" style={{padding: '30px'}} > 
+                <Popconfirm
+                  title="Are you sure want to submit this?"
+                  onConfirm={confirm}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button
+                  type="primary"
+                  shape="round"
+                  size={'large'}
+                  style={{ background: "#d3d3d3", borderColor: "#d3d3d3" }} 
+                  >
+                    create 
+                  </Button>
+                </Popconfirm>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
   }
 }
 
